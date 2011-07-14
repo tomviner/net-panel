a46c3c82f5bafe12b51aa203b0c7d38ae3cec78e
TomV <tom@viner.tv>
2011-07-14 19:23:08 +0100

    add class

--- ping.py
+++ ping.py
@@ -3,23 +3,23 @@ import subprocess
 import time
 
 
-def test_connection():
-    p = subprocess.Popen(
-        ('ping', '-c 1', '-n', '8.8.8.8'), 
-        stdout=subprocess.PIPE) 
-    stdout = p.communicate()[0]
-    if not p.returncode:
-        ptime = re.findall(r' (time=[\d.]+ ms)', stdout)
-        if ptime:
-            return ptime[0]
-    return False
+class Ping(object):
+    def test_connection():
+        p = subprocess.Popen(
+            ('ping', '-c 1', '-n', '8.8.8.8'),
+            stdout=subprocess.PIPE)
+        stdout = p.communicate()[0]
+        if not p.returncode:
+            ptime = re.findall(r' (time=[\d.]+ ms)', stdout)
+            if ptime:
+                return ptime[0]
+        return False
 
+tester = Ping()
 while True:
-    res = test_connection()
+    res = tester.test_connection()
     print res
     if res:
         print 'Net Found', 
         break
-            
     time.sleep(1)
-
