f434a606998728fb63511248b7f969db6f146283
TomV <tom@viner.tv>
2012-01-31 20:59:23 +0000

    refactor update for separate ssid and ping intervals

--- ping.py
+++ ping.py
@@ -28,6 +28,8 @@ def matching_wifi_network(good_ssids=()):
 
 class Ping(object):
     DEFAULT_INTERVAL = 5000
+    MINIMUM_PING_INTERVAL = 1*1000
+    MAXIMUM_PING_INTERVAL = 5*60*1000
 
     UNKNOWN, DISCONNECTED, CONNECTED = range(3)
     icon_filename = {
@@ -40,13 +42,29 @@ class Ping(object):
         self.good_ssids = good_ssids
         self.location = os.path.dirname(os.path.realpath(__file__))
         self.state = self.UNKNOWN
+        self.matching_network = matching_wifi_network(self.good_ssids)
         self.update_icon()
         self.icon.connect('activate', self.icon_click)
         self.icon.set_tooltip("Waiting for data...")
         self.icon.set_visible(True)
         self.tick_interval = self.DEFAULT_INTERVAL #number of ms between each poll
+        self.ssid_interval = self.DEFAULT_INTERVAL
+        self.ping_interval = self.DEFAULT_INTERVAL
         self.down_timestamp = time.time()
         self.last_down_duration = 0 #secs
+
+        self.last_ping = 0
+        self.last_ssid = 0
+
+    @property
+    def unknown(self):
+        return self.state == self.UNKNOWN
+    @property
+    def connected(self):
+        return self.state == self.CONNECTED
+    @property
+    def disconnected(self):
+        return self.state == self.DISCONNECTED
 
     @staticmethod
     def backoff(t, direction, factor=2, mn=5*1000, mx=5*60*1000):
@@ -105,11 +123,37 @@ class Ping(object):
         """
         This method is called everytime a tick interval occurs
         """
+        print
+        now = time.time()
+        def st(t):
+            return time.asctime(time.gmtime(t))
+
+        print '\tnow', st(now)
+        print '\tping', st(self.last_ping), 'interval:', self.ping_interval/1000.0
+        print now, self.last_ping + self.ping_interval/1000.0, -now + self.last_ping + self.ping_interval/1000.0
+        print '\tssid', st(self.last_ssid), 'interval:', self.ssid_interval/1000.0
+        print now , self.last_ssid + self.ssid_interval/1000.0, -now + self.last_ssid + self.ssid_interval/1000.0
+        #import pdb
+        #pdb.set_trace()
+        if self.unknown or self.last_ping + self.ping_interval/1000.0 < now :
+            print 'update ping'
+            self.update_ping()
+            self.update_icon()
+            self.last_ping = time.time()
+        if not self.last_ssid or self.last_ssid + self.ssid_interval/1000.0 < now:
+            print 'update ssid'
+            self.matching_network = matching_wifi_network(self.good_ssids)
+            self.last_ssid = time.time()
+        self.adjust_intervals()
+        gobject.timeout_add(self.tick_interval, self.update)
+        print 'gobject.timeout_add(%s, %s)' % (self.tick_interval, self.update)
+
+    def update_ping(self):
         res = self.test_connection()
         tooltip = []
         if res:
             # if moving from DIS to CONNECTED, store how long connection was down
-            if self.state == self.DISCONNECTED:
+            if self.disconnected:
                 self.last_down_duration = time.time()-self.down_timestamp
 
             self.state = self.CONNECTED
@@ -123,7 +167,7 @@ class Ping(object):
                 tooltip.append('(last downtime lasted=%.0f secs)' % self.last_down_duration)
         else:
             # if moving from CONN to DISCONNECTED, store timestamp of loss 
-            if self.state == self.CONNECTED:
+            if self.connected:
                 self.down_timestamp = time.time()
 
             self.state = self.DISCONNECTED
@@ -131,14 +175,11 @@ class Ping(object):
             print 'Net Not Found'
             self.icon.set_tooltip('Lost %.0f secs ago' % (time.time()-self.down_timestamp))
 
-        self.update_icon()
-        self.adjust_tick_interval()
-        gobject.timeout_add(self.tick_interval, self.update)
         if self.tick_interval != self.DEFAULT_INTERVAL:
             tooltip.append('Interval: %s' % self.pretty_interval)
 
         tooltip = ' '.join(tooltip)
-        print tooltip
+        print 'tooltip', tooltip
         self.icon.set_tooltip(tooltip)
 
     @property
@@ -151,25 +192,38 @@ class Ping(object):
         return '%3.0f %s' % (float(t), unit)
 
 
-    def adjust_tick_interval(self):
+    def adjust_intervals(self):
         """
         Check whether we're on a matching network and the current state
         and possibly apply exponential backoff to the interval time
         """
-        matching_network = matching_wifi_network(self.good_ssids)
-        state = self.state
-        direction = None
-        if not matching_network:
-            print 'not matching network'
-            direction = 1
-        else:
+        if self.matching_network:
             print 'matching network'
-            direction = -1
-        if direction:
-            print 'update', self.tick_interval,
-            tick = self.backoff(self.tick_interval, direction)
-            self.tick_interval = tick
-            print '-->', tick
+            if self.connected or self.unknown:
+                # look for tunnels
+                self.ping_interval = self.DEFAULT_INTERVAL
+                self.ssid_interval = self.DEFAULT_INTERVAL
+            else:
+                # look for the end of tunnels
+                # disconnected pings use no external bandwidth
+                self.ping_interval = self.MINIMUM_PING_INTERVAL
+                self.ssid_interval = self.DEFAULT_INTERVAL
+        else:
+            print 'not matching network because self.matching_network=', self.matching_network, 'from matching_wifi_network(self.good_ssids)=', matching_wifi_network(self.good_ssids)
+            if self.connected or self.unknown:
+                # stop pinging, await switch to matching network 
+                self.ping_interval = self.MAXIMUM_PING_INTERVAL
+                self.ssid_interval = self.DEFAULT_INTERVAL
+            else:
+                # stop pinging, await matching network to connect
+                self.ping_interval = self.MINIMUM_PING_INTERVAL
+                self.ssid_interval = self.DEFAULT_INTERVAL
+
+        print 'update interval', self.tick_interval/1000.0,
+        #tick = self.backoff(self.tick_interval, direction)
+        tick = min(self.ping_interval, self.ssid_interval)
+        self.tick_interval = tick
+        print '-->', tick/1000.0
 
     def main(self):
         """
